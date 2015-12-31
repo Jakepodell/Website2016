ee2a3e464349cdb84be906e3486a3f12155e8331
Jake Podell <podell6@gmail.com>
2015-12-31 01:53:26 -0500

    javascript cleaning and commenting, description header hover styles

--- js/site.work.js
+++ js/site.work.js
@@ -11,8 +11,6 @@ var Site = Site || {};
 
 Site.work = {
 
-    $iphone: $("#iphone"), // init these in an array and do them all simultaneously
-    $iphonr_text: $("#iphone_text"),
     $work: $("#work"),
 
 
@@ -20,13 +18,15 @@ Site.work = {
      * initialize the work section of the site, including image and text
      */
     init: function () {
-        this.init_iphone_scroll();
+        this.init_work_item_scroll();
     },
 
     /**
      * initialize the onScroll function for the work preview animations
+     * go through each work item and define the visibility of its elements
+     * based on the vertical scroll distance
      */
-    init_iphone_scroll: function () {
+    init_work_item_scroll: function () {
         window.onscroll = function() {
 
             $(".work_item").each(function(){
@@ -36,20 +36,24 @@ Site.work = {
                 var image_scroll_top = $image.position().top;
                 var vertical_scroll_position = $(document).scrollTop()+(window.innerHeight*0.5);
                 var offset_difference = vertical_scroll_position - (image_scroll_top - parseInt(Site.work.$work.css("padding-top")));
+                //Behavior for the 100px transition region
                 if (offset_difference >= 0 && offset_difference <= 100) {
                     $image.css("opacity" , ""+offset_difference*0.01);
                     $image.css("transform" , "translateX("+((offset_difference/2-25)*($image.hasClass("slide_from_left") ? 1 : -1))+"px)"); //25 =initial transform
                 }
+                //Behavior for before the transition region has been reached
                 else if(offset_difference<0){
                     $image.css("opacity" , "0");
                     $image.css("transform" , "translateX("+($image.hasClass("slide_from_left") ? -2 : 2)+"5px)");
                     $hr.css("opacity","0");
                 }
+                //Behavior for after the transion region has been passed
                 else if(offset_difference>100){
                     $image.css("opacity" , "1");
                     $image.css("transform" , "translateX("+($image.hasClass("slide_from_left") ? 2 : -2)+"5px)");
                     $hr.css("opacity","0.5");
                 }
+                //Show or hide the description based on if the transion region has been passed
                 $description.css("opacity","" + offset_difference>100 ? 1 : 0);
                 $description.css("transform","translateY(" + (offset_difference>100 ? 0 : 15) + "px)");
             })
